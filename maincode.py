db32df790b4cf7200b37f0d2713c424547d45b6e
Sharafdeen Quadri <61978356+Olalekan2040-slack@users.noreply.github.com>
2022-10-14 16:26:51 -0800

    Create maincode.py

---
+++ maincode.py
@@ -0,0 +1,7 @@
+ def twoSum(self, nums: List[int], target: int) -> list[int]:
+            prevMap = {} #val : index
+            for i, n in enumerate(nums):
+                diff = target - n
+                if diff in prevMap:
+                    return [prevMap[diff], i]
+                prevMap[n] = i
